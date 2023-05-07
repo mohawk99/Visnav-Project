3941baccc4cd7ec35dd3be73d5dfc599efda478c
s0083 <ge86zab@mytum.de>
2023-05-07 20:42:25 +0200

    return

--- include/visnav/ex1.h
+++ include/visnav/ex1.h
@@ -44,12 +44,12 @@ Eigen::Matrix<T, 3, 3> user_implemented_expmap(
     const Eigen::Matrix<T, 3, 1>& xi) {
   // TODO SHEET 1: implement
 
-  Eigen::Matrix<T, 3, 1> theta = abs(xi)
-  Eigen::Matrix3d expS = Eigen::Matrix3d::Identity() + (xi / theta) * sin(theta) + (xi * xi) / (theta * theta) * (1 - cos(theta)); 
+  Eigen::Matrix<T, 3, 1> theta = abs(xi) Eigen::Matrix3d expS =
+      Eigen::Matrix3d::Identity() + (xi / theta) * sin(theta) +
+      (xi * xi) / (theta * theta) * (1 - cos(theta));
 
-
- // UNUSED(xi);
-  return Eigen::Matrix<T, 3, 3>();
+  // UNUSED(xi);
+  return expS;
 }
 
 // Implement log for SO(3)

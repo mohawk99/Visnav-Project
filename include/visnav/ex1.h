c3c071ca0a5edf65ae74b28dea4be21823832261
s0083 <ge86zab@mytum.de>
2023-05-14 20:16:49 +0200

    ..

--- include/visnav/ex1.h
+++ include/visnav/ex1.h
@@ -55,12 +55,14 @@ Eigen::Matrix<T, 3, 3> user_implemented_expmap(
   double theta = xi.norm();
   Eigen::Matrix<T, 3, 3> I;
   I.setIdentity();
-  Eigen::Matrix<T, 3, 3> expS = I + ((w_hat / theta) * sin(theta)) + (((w_hat * w_hat) / (theta * theta)) * (1 - cos(theta))); 
-  if(theta == 0){
+  Eigen::Matrix<T, 3, 3> expS =
+      I + ((w_hat / theta) * sin(theta)) +
+      (((w_hat * w_hat) / (theta * theta)) * (1 - cos(theta)));
+  if (theta == 0) {
     expS.setIdentity();
   }
 
- // UNUSED(xi);
+  // UNUSED(xi);
   return expS;
 }
 
@@ -69,16 +71,16 @@ template <class T>
 Eigen::Matrix<T, 3, 1> user_implemented_logmap(
     const Eigen::Matrix<T, 3, 3>& mat) {
   // TODO SHEET 1: implement
-  double theta = acos((mat.trace() - 1)/2);
-  double k = (theta/(2 * sin(theta)));
+  double theta = acos((mat.trace() - 1) / 2);
+  double k = (theta / (2 * sin(theta)));
   Eigen::Matrix<T, 3, 1> log;
-  log(0,0) = (mat(2,1) - mat(1,2)) * k;
-  log(1,0) = (mat(0,2) - mat(2,0)) * k;
-  log(2,0) = (mat(1,0) - mat(0,1)) * k;
-  if(theta == 0){
-    log(0,0) = log(1,0) = log(2,0) = 0;
+  log(0, 0) = (mat(2, 1) - mat(1, 2)) * k;
+  log(1, 0) = (mat(0, 2) - mat(2, 0)) * k;
+  log(2, 0) = (mat(1, 0) - mat(0, 1)) * k;
+  if (theta == 0) {
+    log(0, 0) = log(1, 0) = log(2, 0) = 0;
   }
- // UNUSED(mat);
+  // UNUSED(mat);
   return log;
 }
 
@@ -89,11 +91,11 @@ Eigen::Matrix<T, 4, 4> user_implemented_expmap(
   // TODO SHEET 1: implement
   Eigen::Matrix<T, 3, 1> w;
   Eigen::Matrix<T, 3, 1> v;
-  for(int i=1; i<4; i++){
-    v(i-1,0) = xi(i-1,0); 
+  for (int i = 1; i < 4; i++) {
+    v(i - 1, 0) = xi(i - 1, 0);
   }
-  for(int i=1; i<4; i++){
-    w(i-1,0) = xi(i+2,0); 
+  for (int i = 1; i < 4; i++) {
+    w(i - 1, 0) = xi(i + 2, 0);
   }
   Eigen::Matrix<T, 3, 3> w_hat;
   w_hat.setZero();
@@ -106,22 +108,25 @@ Eigen::Matrix<T, 4, 4> user_implemented_expmap(
   double theta = w.norm();
   Eigen::Matrix<T, 3, 3> I;
   I.setIdentity();
-  Eigen::Matrix<T, 3, 3> exp = I + ((w_hat / theta) * sin(theta)) + (((w_hat * w_hat) / (theta * theta)) * (1 - cos(theta))); 
-  Eigen::Matrix<T, 3, 3> J = Eigen::Matrix3d::Identity() + w_hat / (theta * theta) * (1 - cos(theta)) + ((w_hat * w_hat)/(theta * theta * theta)) * (theta - sin(theta));
-      if(w.isZero()){
+  Eigen::Matrix<T, 3, 3> exp =
+      I + ((w_hat / theta) * sin(theta)) +
+      (((w_hat * w_hat) / (theta * theta)) * (1 - cos(theta)));
+  Eigen::Matrix<T, 3, 3> J =
+      Eigen::Matrix3d::Identity() + w_hat / (theta * theta) * (1 - cos(theta)) +
+      ((w_hat * w_hat) / (theta * theta * theta)) * (theta - sin(theta));
+  if (w.isZero()) {
     exp.setIdentity();
     J.setIdentity();
   }
   Eigen::Matrix<T, 3, 1> jv = J * v;
   Eigen::Matrix<T, 4, 4> expS;
-  expS.setIdentity(); 
+  expS.setIdentity();
   expS.template block<3, 3>(0, 0) = exp;
   expS.template block<3, 1>(0, 3) = jv;
   // expS[4][1] = expS[4][2] = expS[4][3] = 0;
   // expS[4][4] = 1;
 
-
- // UNUSED(xi);
+  // UNUSED(xi);
   return expS;
 }
 
@@ -134,13 +139,13 @@ Eigen::Matrix<T, 6, 1> user_implemented_logmap(
   R = mat.template block<3, 3>(0, 0);
   Eigen::Matrix<T, 3, 1> t;
   t = mat.template block<3, 1>(0, 3);
-  double theta = acos((R.trace() - 1.0)/2.0);
-  double k = (theta/(2*sin(theta)));
+  double theta = acos((R.trace() - 1.0) / 2.0);
+  double k = (theta / (2 * sin(theta)));
   Eigen::Matrix<T, 3, 1> w;
-  w(0,0) = (R(2,1) - R(1,2)) * k;
-  w(1,0) = (R(0,2) - R(2,0)) * k;
-  w(2,0) = (R(1,0) - R(0,1)) * k;
-  if(theta == 0){
+  w(0, 0) = (R(2, 1) - R(1, 2)) * k;
+  w(1, 0) = (R(0, 2) - R(2, 0)) * k;
+  w(2, 0) = (R(1, 0) - R(0, 1)) * k;
+  if (theta == 0) {
     w.setZero();
   }
   Eigen::Matrix<T, 3, 3> w_hat;
@@ -153,8 +158,12 @@ Eigen::Matrix<T, 6, 1> user_implemented_logmap(
   w_hat(2, 1) = w(0);
   Eigen::Matrix<T, 3, 3> I;
   I.setIdentity();
-  Eigen::Matrix<T, 3, 3> J_inv = I - (w_hat/2.0) + ((1.0/(theta*theta)) - ((1.0 + cos(theta))/(2*theta*sin(theta)))) * (w_hat * w_hat);
-  if(theta == 0){
+  Eigen::Matrix<T, 3, 3> J_inv =
+      I - (w_hat / 2.0) +
+      ((1.0 / (theta * theta)) -
+       ((1.0 + cos(theta)) / (2 * theta * sin(theta)))) *
+          (w_hat * w_hat);
+  if (theta == 0) {
     J_inv.setIdentity();
   }
   Eigen::Matrix<T, 3, 1> v = J_inv * t;
@@ -163,7 +172,7 @@ Eigen::Matrix<T, 6, 1> user_implemented_logmap(
   log.template block<3, 1>(3, 0) = w;
 
   // UNUSED(mat);
-  return log;   
+  return log;
 }
 
 }  // namespace visnav
